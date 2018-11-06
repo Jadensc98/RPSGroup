6c6f66b227a4c73dfc37af7d6a08c3add3522c1a
ERIN CARTER <carter.erin86@gmail.com>
2018-11-06 10:02:21 -0500

    new version

---
+++ assets/script/site.js
@@ -0,0 +1,9 @@
+ $('#rock').click(function() {
+        makeUserChoice("rock");
+    });
+    $('#paper').click(function() {
+        makeUserChoice("paper");
+    });
+    $('#scissors').click(function() {
+        makeUserChoice("scissors");
+    });
