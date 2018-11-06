6f2ac1a29e0d0c92281fc65890ebc9a5297d1932
ERIN CARTER <carter.erin86@gmail.com>
2018-11-06 10:13:51 -0500

    final version

---
+++ assets/script/site2.js
@@ -0,0 +1,71 @@
+
+$(function(){
+
+var computerScore=0,
+  playerScore=0;
+function setText(txt){
+$("#playerScore").text(playerScore);
+$("#computerScore").text(computerScore);
+$("#result").text(txt);
+}
+
+function makeComputerChoice(){
+  var computerChoice = "";
+  var r = Math.random();
+  if (r < .33) {
+    computerChoice = "rock";
+  } else if (r < .66) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";   
+  };
+  return computerChoice;
+ }
+
+
+function makeUserChoice(userChoice){
+  
+  var computerChoice = makeComputerChoice();
+  var outcome = "";
+if (userChoice == computerChoice) {
+    outcome="Tie! LAME.";
+} else if (userChoice == "rock") {
+    if (computerChoice == "scissors") {
+        outcome="BATTLE WON!"; playerScore=playerScore+1;
+    } else {
+        outcome="battle lost."; computerScore=computerScore+1;
+    };
+} else if (userChoice == "paper") {
+    if (computerChoice == "rock") {
+        outcome="BATTLE WON!"; playerScore=playerScore+1;
+    } else {
+        outcome="battle lost."; computerScore=computerScore+1;
+    };
+} else if (userChoice == "scissors") {
+    if (computerChoice == "paper") {
+        outcome="BATTLE WON!"; playerScore=playerScore+1;
+    } else {
+        outcome="battle lost."; computerScore=computerScore+1;
+    };
+};
+  setText(userChoice+" vs "+computerChoice+" = "+outcome);
+};
+// if (playerScore==5){
+// 	outcome="YOU WON THE WAR!"
+// } else if (computerScore==5){
+// 	outcome="YOU LOST THE WAR..."
+// } else {
+// 	outcome="YOU LOST THE WAR..."
+// };
+
+ 
+//  $('#rock').click(function() {
+//         makeUserChoice("rock");
+//     });
+//     $('#paper').click(function() {
+//         makeUserChoice("paper");
+//     });
+//     $('#scissors').click(function() {
+//         makeUserChoice("scissors");
+//     });
+// });
