0a80d219ba43c09b1ea67eba0a57c32f5426d8ff
bhanna1693 <bhanna1693@gmail.com>
2019-01-02 10:21:12 -0500

    second commit

--- DevOps/shell_scripting/gitcommit.sh
+++ DevOps/shell_scripting/gitcommit.sh
@@ -1,5 +1,5 @@
-#!/bin/bash -e
-commit_message="$1"
-git add . -A
-git commit -m "$commit_message"
-git push
+#!/bin/bash  
+git add .  
+read -p "Commit description: " desc  
+git commit -m "$desc"
+git push origin master
